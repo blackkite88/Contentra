bc4af979f0271de9274b1a218bf068b819173bed
vivek <139089625+blackkite88@users.noreply.github.com>
2026-05-08 21:12:26 +0530

    Configure CORS with specific options

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ import connectDB from "./configs/db.js";
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: "https://contentra-psi.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(clerkMiddleware());
 
